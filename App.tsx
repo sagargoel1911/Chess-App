1d2d1bd3bec65891f7204444e578db547ca8c504
Sagar Goel <sagargoel@wizcommerce.com>
2024-04-29 13:46:39 +0530

    moving pieces to different tiles

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { NativeBaseProvider } from 'native-base';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import theme from './src/utils/theme';
 import AppStack from './src/navigation/AppStack';
@@ -41,14 +42,16 @@ const App = () => {
 	return (
 		<Provider store={store}>
 			<NavigationContainer>
-				<SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
-					<NativeBaseProvider>
-						<PersistGate loading={null} persistor={persistor}>
-							<StatusBar />
-							<AppStack />
-						</PersistGate>
-					</NativeBaseProvider>
-				</SafeAreaView>
+				<GestureHandlerRootView style={{ flex: 1, backgroundColor: theme.colors.white }}>
+					<SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
+						<NativeBaseProvider>
+							<PersistGate loading={null} persistor={persistor}>
+								<StatusBar />
+								<AppStack />
+							</PersistGate>
+						</NativeBaseProvider>
+					</SafeAreaView>
+				</GestureHandlerRootView>
 			</NavigationContainer>
 		</Provider>
 	);
--- src/screens/Game/components/Board/Board.tsx
+++ src/screens/Game/components/Board/Board.tsx
@@ -1,15 +1,42 @@
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import _ from 'lodash';
+import { useState } from 'react';
 
 import Tile from './components/Tile';
-import { files, ranks } from './constants';
+import { files, initial_position, ranks } from './constants';
+import Piece from './components/Piece';
+
+const styles = StyleSheet.create({
+	board: {
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+	},
+});
 
 const Board = () => {
+	const [current_position, set_current_position] = useState<any>(initial_position);
+
+	const change_position = (new_rank: number, new_file: number, rank: number, file: number) => {
+		const new_position = _.cloneDeep(current_position);
+		new_position[rank][file] = '';
+		new_position[new_rank][new_file] = current_position[rank][file];
+		set_current_position(new_position);
+	};
+
 	return (
-		<View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+		<View style={styles.board}>
 			{_.map(ranks, (rank: number) => {
 				return _.map(files, (file: number) => {
 					return <Tile key={`${file}${rank}`} file={file} rank={rank} />;
+				});
+			})}
+			{_.map(ranks, (rank: number) => {
+				return _.map(files, (file: number) => {
+					if (current_position[rank][file] === '') {
+						return null;
+					} else {
+						return <Piece rank={rank} file={file} current_position={current_position} change_position={change_position} />;
+					}
 				});
 			})}
 		</View>
